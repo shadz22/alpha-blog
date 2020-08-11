5c9088f2eff716a14aa6f36a629b0f69392a7727
shadz22 <shadz22@yahoo.co.uk>
2020-08-11 14:05:51 +0100

    Update the create action in articles controller so the logged in user is associated with the article being created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @articles = @user.articles.paginate(page: params[:page], per_page: 2)
+    @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def index
