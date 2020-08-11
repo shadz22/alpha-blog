36e06fd45daeeaeb26e01b7a87b00f4e9a41edc7
shadz22 <shadz22@yahoo.co.uk>
2020-08-11 20:53:47 +0100

    Restrict activities in the articles controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,11 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You need to be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
+
 end
