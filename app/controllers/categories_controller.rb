de9374c5f3c05d687a2304593624693ffc4c3700
shadz22 <shadz22@yahoo.co.uk>
2020-08-19 10:53:46 +0100

    Initiate @articles instance variable in categories controller show action to capture all articles associated with a specific category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,7 @@ before_action :require_admin, except: [:index, :show]
   
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def index
